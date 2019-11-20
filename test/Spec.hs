a36cf030c0df657956b81b8427eab65c713eecde
Andrius Bentkus <andrius.bentkus@gmail.com>
2019-11-20 18:36:53 +0200

    remove unused functions

--- test/Spec.hs
+++ test/Spec.hs
@@ -17,14 +17,6 @@ parserTests = [
   parseJsonValue "123" (JsonInt 123),
   parseJsonValue "\"abc\"" (JsonString "abc")]
 
-parseJsonInt string int = testCase ("Parse json value '" ++ string ++ "' successfully")
-  (assertEqual "should parse json int" (getValue $ apply jvalue string) (JsonInt int))
-
-
-parseJsonString :: String -> String -> TestTree
-parseJsonString jsonString expectedString = testCase ("Parse json string '" ++ jsonString ++ "' to '" ++ expectedString ++ "'")
-  (assertEqual "should parse json string" (getValue $ apply jstring jsonString) (JsonString expectedString))
-
 parseJsonValue :: String -> JsonValue -> TestTree
 parseJsonValue jsonString expectedJsonValue = testCase ("Parse json string '" ++ jsonString ++"' and get " ++ (show expectedJsonValue))
   (assertEqual "should parse json string" (getValue $ apply jvalue jsonString) expectedJsonValue)
